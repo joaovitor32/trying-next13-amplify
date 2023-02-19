f34a42425a2bb2e4e0f8e1dd52969679aa58ed9c
Joao Vitor Muniz Lopes <joao.lopes@mundiale.com.br>
2023-02-19 10:25:32 -0300

    refactor: add changes

--- pages/index.js
+++ pages/index.js
@@ -5,11 +5,11 @@ import { useQuery } from '@tanstack/react-query';
 import getIp from '../globals/services/ip';
 
 export default function Home({ ip }) {
-  const { data, isFetched } = useQuery({ queryKey: ['ip'], queryFn: getIp });
+  //const { data, isFetched } = useQuery({ queryKey: ['ip'], queryFn: getIp });
 
-  const geo = data?.geo;
+  //const geo = data?.geo;
   //const ip = data?.ip ?? 'Ip não detectado';
-  const headers = data?.headers;
+  //const headers = data?.headers;
 
   return (
     <div className={styles.container}>
@@ -21,10 +21,10 @@ export default function Home({ ip }) {
       {isFetched ? (
         <div>
           <p className={styles.ip}>IP: {ip}</p>
-          <p className={styles.geo}>Cidade: {geo?.city ?? 'Cidade não detectada'}</p>
+          {/*<p className={styles.geo}>Cidade: {geo?.city ?? 'Cidade não detectada'}</p>
           <p className={styles.geo}>País: {geo?.country ?? 'País não detectado'}</p>
           <p className={styles.geo}>Região: {geo?.region ?? 'Região não detectado'}</p>
-          <p className={styles.geo}>Headers: {JSON.stringify(headers)}</p>
+          <p className={styles.geo}>Headers: {JSON.stringify(headers)}</p>*/}
         </div>
       ) : null}
     </div>
